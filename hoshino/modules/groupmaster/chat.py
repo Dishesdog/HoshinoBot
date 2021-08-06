c737ced788f94989a2a3cd47d2e3e13d4526aa14
lulu <794329728@qq.com>
2021-08-06 20:03:31 +0800

    fix

--- hoshino/modules/groupmaster/chat.py
+++ hoshino/modules/groupmaster/chat.py
@@ -40,11 +40,6 @@ async def chat_mua(bot, ev):
     await bot.send(ev, '笨蛋~', at_sender=True)
 
 
-@sv.on_fullmatch('来点星奏')
-async def seina(bot, ev):
-    await bot.send(ev, R.img('星奏.png').cqcode)
-
-
 @sv.on_fullmatch('我有个朋友说他好了', '我朋友说他好了')
 async def ddhaole(bot, ev):
     await bot.send(ev, '那个朋友是不是你弟弟？')
@@ -66,12 +61,6 @@ async def chat_queshi(bot, ctx):
         await bot.send(ctx, R.img('确实.jpg').cqcode)
 
 
-@sv.on_keyword('会战')
-async def chat_clanba(bot, ctx):
-    if random.random() < 0.02:
-        await bot.send(ctx, R.img('我的天啊你看看都几度了.jpg').cqcode)
-
-
 @sv.on_keyword('内鬼')
 async def chat_neigui(bot, ctx):
     if random.random() < 0.10:
@@ -83,6 +72,7 @@ nyb_player = f'''{R.img('newyearburst.gif').cqcode}
 ──●━━━━ 1:05/1:30
 ⇆ ㅤ◁ ㅤㅤ❚❚ ㅤㅤ▷ ㅤ↻
 '''.strip()
+
 
 @sv.on_keyword('春黑', '新黑')
 async def new_year_burst(bot, ev):
